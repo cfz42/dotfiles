9ff7d94f1c1276188bf4702d01db3a4930dd1a2b
Clément FERNANDEZ <clement.frndz@gmail.com>
2018-04-05 23:11:57 +0200

    Changes from arch desktop

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 for entry in ./symlink/*
 do
-	filename=${entry:10} # ${parameter:offset} (substring)
-	if [[ -e ~/."$filename" ]]; then # if $filename exists
-		mv ~/."$filename" ~/."$filename".backup # Save original file
-	fi
+	filename=${entry:10}
+	# if [[ -e ~/."$filename" ]]; then # if $filename exists
+		# mv ~/."$filename" ~/."$filename".backup # Save original file
+	# fi
 	ln -s "$PWD"/symlink/"$filename" ~/."$filename" # Create link to ./symlink/$filename
 done
